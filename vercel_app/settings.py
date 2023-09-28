4c97555b8326f33897f4fa3d589de202acb2bdc9
ttwrpz <smrmakeyt@gmail.com>
2023-09-28 23:14:03 +0700

    Test CSS #5

--- vercel_app/settings.py
+++ vercel_app/settings.py
@@ -115,10 +115,14 @@ USE_TZ = True
 
 STATIC_URL = 'static/'
 
+# Add these new lines
+STATICFILES_DIRS = (
+    os.path.join(BASE_DIR, 'static'),
+)
+
+STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
+
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.1/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
-
-import mimetypes
-mimetypes.add_type("text/css", ".css", True)
