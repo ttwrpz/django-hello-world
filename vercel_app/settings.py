7e70e9e914d11e5fc0c215aaea1fb608ceb4d2dd
ttwrpz <smrmakeyt@gmail.com>
2023-09-28 23:05:14 +0700

    Test CSS #3

--- vercel_app/settings.py
+++ vercel_app/settings.py
@@ -119,3 +119,6 @@ STATIC_URL = 'static/'
 # https://docs.djangoproject.com/en/4.1/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+
+import mimetypes
+mimetypes.add_type("text/css", ".css", True)
