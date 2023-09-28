4b876f7048087a8a770f1579d29945cf6bedcc48
ttwrpz <smrmakeyt@gmail.com>
2023-09-28 22:51:19 +0700

    Test

--- example/views.py
+++ example/views.py
@@ -2,15 +2,8 @@
 from datetime import datetime
 
 from django.http import HttpResponse
+from django.template import loader
 
 def index(request):
-    now = datetime.now()
-    html = f'''
-    <html>
-        <body>
-            <h1>Hello from Vercel!</h1>
-            <p>The current time is { now }.</p>
-        </body>
-    </html>
-    '''
-    return HttpResponse(html)
+    template = loader.get_template('base.html')
+    return HttpResponse(template.render())
